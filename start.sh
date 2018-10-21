ba2aad69df6c5cfcd61755b0cf836969127ef10c
Matt Walters <me@mattwalters.net>
2018-10-20 23:00:51 -0400

    Fix PGID checking before modifying group
    Existence of PUID was being checked instead of PGID before modifying
    the group ID.

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env ash
 
 if [ -n "$PUID" ]; then usermod -u $PUID downloader && echo "PUID set to $PUID"; fi
-if [ -n "$PUID" ]; then groupmod -g $PGID downloader && echo "PGID set to $PGID"; fi
+if [ -n "$PGID" ]; then groupmod -g $PGID downloader && echo "PGID set to $PGID"; fi
 
 env > /environment.sh
 sed -i -e 's/^/export /' /environment.sh
